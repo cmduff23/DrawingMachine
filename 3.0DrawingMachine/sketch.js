5ab4d2694b286e4b4ccb4d045c202582ed1430ee
Caitlin <cmduff91@gmail.com>
2021-11-15 00:21:10 -0800

    draw

---
+++ 3.0DrawingMachine/sketch.js
@@ -0,0 +1,39 @@
+let array = []
+
+function setup() {
+  createCanvas(600, 600);
+  background('#01002D');
+
+  strokeWeight(5);
+ 
+}
+
+function draw() {
+ 
+  if (mouseIsPressed) {
+    background('#F0DDFF');
+    stroke(map(mouseX, 0, 600, 0, 255, true))
+    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+    array.push([mouseX, mouseY]);
+  } 
+}
+
+function keyTyped(){
+
+if (key === 's'){
+// save this image
+saveCanvas('fileName','png');
+} else if (key === 'd'){
+  //image display
+
+  beginShape();
+  for(let i = 0; i < array.length - 1; i++){
+    //line(array [i] [0], array [0] [1], array [i + 1] [0], array [i + 1] [1]);
+  curveVertex(array[i][0], array [i][1])
+  }
+  endShape();
+}
+return false;
+
+}
